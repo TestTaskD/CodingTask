f82c2c3fd7fb768b43f5747c4cebcf2482c3e3e3
Denis Alioshyn <>
2023-11-06 07:51:46 +0100

    repository init

---
+++ CodingTask/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  CodingTask
+//
+//  Created by Denis Aleshyn on 06/11/2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
---
+++ CodingTask/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  CodingTask
+//
+//  Created by Denis Aleshyn on 06/11/2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
